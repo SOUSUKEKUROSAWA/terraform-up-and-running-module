323e364c4be747d0f587846151a1425419bd8094
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2024-07-20 14:49:07 +0900

    起動設定のパラメータを変更可能に

--- services/web-server-cluster/main.tf
+++ services/web-server-cluster/main.tf
@@ -92,12 +92,13 @@ resource "aws_security_group_rule" "allow_http_outbound" {
 
 # Auto Scaling Group（ASG） 内のインスタンスの起動設定（起動テンプレートを使うのが一般的 https://docs.aws.amazon.com/ja_jp/autoscaling/ec2/userguide/launch-templates.html）
 resource "aws_launch_configuration" "example" {
-    image_id = "ami-0fb653ca2d3203ac1"
+    image_id = var.ami
     instance_type = var.instance_type
     security_groups = [aws_security_group.instance.id]
 
     # 最初のインスタンス起動時にのみ実行されるスクリプト
     user_data = templatefile("${path.module}/user-data.sh", {
+        server_text = var.server_text
         server_port = var.server_port
         # terraform_remote_stateはDBがデプロイされた状態でないと使えないので注意
         db_address = data.terraform_remote_state.db.outputs.address
