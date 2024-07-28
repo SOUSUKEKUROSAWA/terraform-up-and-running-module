a181a5b5cb7b92d7ccc646e8367314e920187765
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2024-07-28 20:38:17 +0900

    ユーザーデータがデフォルトでは設定されないようにする

--- cluster/asg-rolling-deploy/variables.tf
+++ cluster/asg-rolling-deploy/variables.tf
@@ -62,5 +62,5 @@ variable "health_check_type" {
 variable "user_data" {
     description = "The User Data script to run in each Instance at boot"
     type = string
-    default = ""
+    default = null
 }
